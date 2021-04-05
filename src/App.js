da1b201b399c9f546240bafcca0f941ec804c622
Jon Mullingtons <jmulhern85@gmail.com>
2021-04-05 10:24:14 +0100

    Added basic quote button functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { quoteList } from "./quotelist.js";
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p id="displayedQuote">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -15,13 +17,57 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <button id="clickybutton">Let's get this party started</button>
       </header>
     </div>
   );
 }
 
+//Declare variables
+var quoteArray = "No quote selected yet";
+var quoteText = "No quote selected yet";
+var newQuoteToSelect = "No quote selected yet";
+
+// Count remaining quotes
+var remainingQuotes = quoteList.length;
+console.log("There are " + remainingQuotes + " quotes remaining");
+
+//Select a random quote
+function selectNewQuote() {
+  newQuoteToSelect = Math.floor(Math.random() * quoteList.length);
+  console.log("Selecting array index " + newQuoteToSelect);
+  quoteArray = quoteList[newQuoteToSelect];
+  quoteText = quoteArray[0];
+  console.log(quoteText);
+}
+
+//Change the text to display the new quote
+function changeDisplayedText() {
+  document.getElementById("displayedQuote").innerHTML = quoteText;
+}
+
+//Remove the quote from the list
+function removeDisplayedQuote() {
+  quoteList.splice(newQuoteToSelect, 1);
+  console.log("There are now " + quoteList.length + " quotes remaining");
+}
+
+//Click button for magic to happen
+window.onload=function(){
+const buttonElement = document.getElementById("clickybutton");
+buttonElement.addEventListener("click", () => {
+  if (quoteList.length === 0) {
+    console.log("outta quotes");
+    document.getElementById("displayedQuote").innerHTML = "Out of quotes!";
+  } else {
+    selectNewQuote();
+    changeDisplayedText();
+    removeDisplayedQuote();
+  }
+});
+}
+
 export default App;
 
-//testing first commit
+
---
+++ src/quotelist.js
@@ -0,0 +1,10 @@
+export const quoteList = [
+    ["quote number one", 1],
+    ["quote number two", 1],
+    ["quote number three", 1],
+    ["quote number four", 0],
+    ["quote number five", 0],
+    ["quote number six", 0],
+    ["quote number seven", 0],
+  ];
+  
